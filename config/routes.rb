2576c550fe5053d5f1d8ede352959deea0564856
Glendel Joubert Fyne Acosta <glendelfyne@wssinternational.net>
2022-09-05 15:57:31 +0000

    Removed the Option to "Register" new "Users" and added the "User Authentication" for all the "Application" Controllers and Actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, skip: [:registrations], controllers: { registrations: 'users/registrations' }
+  devise_scope :user do
+    get 'users/edit', to: 'users/registrations#edit', as: 'edit_user_registration'
+    patch 'users', to: 'users/registrations#update', as: 'user_registration'
+    put 'users', to: 'users/registrations#update', as: nil
+  end
 
   namespace :admin do
     resources :users
