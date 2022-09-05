80698336012fe75629bdf05ae31917597f13decd
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2022-09-05 12:14:03 -0500

    Fixes for cloud66

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  devise_for :users, skip: [:registrations], controllers: { registrations: 'users/registrations' }
-  devise_scope :user do
-    get 'users/edit', to: 'users/registrations#edit', as: 'edit_user_registration'
-    patch 'users', to: 'users/registrations#update', as: 'user_registration'
-    put 'users', to: 'users/registrations#update', as: nil
-  end
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   namespace :admin do
     resources :users
