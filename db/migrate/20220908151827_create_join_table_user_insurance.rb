7da2b100479fc6cf48dd15c40d7e053cf91a6c9e
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2022-09-08 10:21:48 -0500

    Added index to Join Table UserInsurance

--- db/migrate/20220908151827_create_join_table_user_insurance.rb
+++ db/migrate/20220908151827_create_join_table_user_insurance.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableUserInsurance < ActiveRecord::Migration[7.0]
   def change
     create_join_table :users, :insurances do |t|
-      # t.index [:user_id, :insurance_id]
+      t.index [:user_id, :insurance_id], unique: true
       # t.index [:insurance_id, :user_id]
     end
   end
