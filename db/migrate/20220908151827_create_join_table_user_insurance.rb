e134316b108d4d110e0ff58e23069e6aebd1d25b
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2022-09-08 10:19:51 -0500

    Files generated and modified by the execution of the command : "rails generate migration CreateJoinTableUserInsurance user insurance"

---
+++ db/migrate/20220908151827_create_join_table_user_insurance.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableUserInsurance < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :users, :insurances do |t|
+      # t.index [:user_id, :insurance_id]
+      # t.index [:insurance_id, :user_id]
+    end
+  end
+end
