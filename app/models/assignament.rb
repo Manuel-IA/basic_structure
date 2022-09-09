64ebd5d7e89e523c4ec249260581b5c8df08ad05
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2022-09-09 08:36:33 -0500

    Added statuses to assignaments

--- app/models/assignament.rb
+++ app/models/assignament.rb
@@ -1,2 +1,16 @@
 class Assignament < ApplicationRecord
+
+  class << self
+    def statuses
+      [
+        { id: 1, name: 'Pending' },
+        { id: 2, name: 'Active' },
+        { id: 3, name: 'Finished' }
+      ]
+    end
+  end
+
+  def status
+    self.class.statuses.find { |status| status[ :id ] === status_id }
+  end
 end
