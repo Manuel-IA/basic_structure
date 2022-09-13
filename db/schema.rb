85e69e67084195e3ece3e1f63be68f6d1488aa39
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2022-09-13 17:20:03 -0500

    "Database Schema" updated by the "AddInsuraceToAssignaments" migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_140032) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_211226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_140032) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "insurance_id"
+    t.index ["insurance_id"], name: "index_assignaments_on_insurance_id"
   end
 
   create_table "assignaments_users", id: false, force: :cascade do |t|
@@ -124,6 +126,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_140032) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "assignaments", "insurances"
   add_foreign_key "notes", "users"
   add_foreign_key "user_details", "users"
 end
