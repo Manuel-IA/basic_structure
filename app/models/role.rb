4ae4412d92d6f8f5fc789b5e4847f7a0fe6e962c
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2022-09-13 08:32:36 -0500

    Moved roles from user_details to role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,12 @@
 class Role < ApplicationRecord
+
+  class << self
+    def role_types
+      [
+        { id: 1, name: 'Manager' },
+        { id: 2, name: 'Specialist' },
+        { id: 3, name: 'Operator' }
+      ]
+    end
+  end
 end
--- app/models/user_details.rb
+++ app/models/user_details.rb
@@ -3,32 +3,6 @@ class UserDetails < ApplicationRecord
   belongs_to :user
 
   class << self
-    def role_types
-      [
-        { id: 1, name: 'Manager' },
-        { id: 2, name: 'Specialist' },
-        { id: 3, name: 'Operator' }
-      ]
-    end
-
-    def roles
-      [
-        { id: 1, role_type_id: 1, name: 'Branch Manager', acronym: 'BM' },
-        { id: 2, role_type_id: 2, name: 'Claim Specialist', acronym: 'CS' },
-        { id: 3, role_type_id: 2, name: 'Client Specialist', acronym: 'CLS' },
-        { id: 4, role_type_id: 2, name: 'Payment Specialist', acronym: 'PS' },
-        { id: 5, role_type_id: 1, name: 'Repair Process Manager', acronym: 'RPM' },
-        { id: 6, role_type_id: 1, name: 'Paint Manager', acronym: 'PM' },
-        { id: 7, role_type_id: 3, name: 'Bodyment Operator', acronym: 'BDOP' },
-        { id: 8, role_type_id: 3, name: 'Prepare Operator', acronym: 'PPOP' },
-        { id: 9, role_type_id: 3, name: 'Detailer Operator', acronym: 'DTOP' },
-        { id: 10, role_type_id: 3, name: 'Scanner Operator', acronym: 'SCOP' },
-        { id: 11, role_type_id: 3, name: 'Painter Operator', acronym: 'PTOP' },
-        { id: 12, role_type_id: 3, name: 'Sanding Operator', acronym: 'SDOP' },
-        { id: 13, role_type_id: 3, name: 'Mechanic Operator', acronym: 'MCOP' },
-        { id: 14, role_type_id: 3, name: 'Electronic Operator', acronym: 'ETOP' }
-      ]
-    end
 
     def statuses
       [
